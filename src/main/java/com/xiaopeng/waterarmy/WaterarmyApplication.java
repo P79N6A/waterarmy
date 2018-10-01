8863ba18300d3cece9742809109ea6cc6178e63c
wenlong.cwl <1206401391@qq.com>
2018-10-01 15:22:29 +0800

    mapper修改
    mapper修改

--- src/main/java/com/xiaopeng/waterarmy/WaterarmyApplication.java
+++ src/main/java/com/xiaopeng/waterarmy/WaterarmyApplication.java
@@ -1,9 +1,11 @@
 package com.xiaopeng.waterarmy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xiaopeng.waterarmy.model.mapper")
 public class WaterarmyApplication {
 
 	public static void main(String[] args) {
