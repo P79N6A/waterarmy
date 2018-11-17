706747718e7e7aed4db0f579a933cd401f59e353
zhangyong <zhangyong1924@cvte.com>
2018-11-17 18:17:17 +0800

    [modify]更新read方法，服务器不再报错

--- src/main/java/com/xiaopeng/waterarmy/common/util/HtmlReadUtil.java
+++ src/main/java/com/xiaopeng/waterarmy/common/util/HtmlReadUtil.java
@@ -1,53 +1,39 @@
 package com.xiaopeng.waterarmy.common.util;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-
+/**
+ * @author zhangyong
+ */
 public class HtmlReadUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(HtmlReadUtil.class);
 
-    private static ThreadFactory threadFactory = new ThreadFactoryBuilder()
-            .setNameFormat("HtmlReadUtil").build();
-
-    private static final LinkedBlockingQueue<String> URLS = new LinkedBlockingQueue<>(100000);
-    private static List<ChromeDriver> chromeDrivers = new ArrayList<>();
-
-    static {
-        for (int i = 0; i < 10; i++) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
-            ChromeDriver chromeDriver = new ChromeDriver(options);
-            new Thread(() -> {
-                while (true) {
-                    try {
-                        String readUrl = URLS.take();
-                        chromeDriver.get(readUrl);
-                        logger.info("阅读：" + readUrl);
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (Exception e) {
-                        logger.error("阅读失败", e);
-                        chromeDriver.close();
-                        chromeDrivers.remove(chromeDriver);
-                    }
-                }
-            }).start();
-            chromeDrivers.add(chromeDriver);
+    private static ThreadLocal<ChromeDriver> chromeDriverThreadLocal = ThreadLocal.withInitial(() -> {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+        try {
+            return new ChromeDriver(options);
+        } catch (Exception e) {
+            logger.error("init ChromeDriver error!!!!!!!!!!!", e);
         }
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> chromeDrivers.forEach(RemoteWebDriver::close)));
-    }
+        return null;
+    });
+
 
     public static void read(String url) {
-        URLS.add(url);
+        try {
+            chromeDriverThreadLocal.get().get(url);
+            String title = chromeDriverThreadLocal.get().getTitle();
+            logger.info("Read url " + title + "  " + url + " successful!!!!!!!!! ");
+        } catch (Exception e) {
+            logger.error("read error!!!!!!!!!!!", e);
+        }
     }
 }
